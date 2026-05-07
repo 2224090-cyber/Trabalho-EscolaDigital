ad563f8ee2b7fca145bf13e911c8080b68125589
2224090-cyber <2224090@alunos.escoladigital.com>
2026-05-07 11:36:39 +0100

    07/05/26

--- Projeto-Final/Horazon_Bank _projetoFinal/criar conta.cs
+++ Projeto-Final/Horazon_Bank _projetoFinal/criar conta.cs
@@ -21,5 +21,20 @@ namespace Horazon_Bank__projetoFinal
         {
 
         }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
